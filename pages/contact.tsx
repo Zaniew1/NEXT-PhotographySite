15ab3061948eee85c0b43efce9582ac0a8f7a227
Mateusz <m.zaniewski1995@gmail.com>
2022-12-30 17:53:43 +0100

    change style contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,7 @@
 import { ContactForm } from "../Components/Form/ContactForm";
 import { Header } from './../Components/Header/Header';
 import { Footer} from './../Components/Footer/Footer';
+import { BackgroundImage } from "../Components/UI/BackgroundImage";
 
 import classes from './contact.module.css';
 import Image from 'next';
@@ -8,28 +9,27 @@ import Image from 'next';
 const Contact:React.FC = (props) => { return (
     <main>
       <Header/>
-     <div className={classes.contact__background}>
-        {/* <Image src="/../../public/img/1.jpg" alt="asd" layout="fill" objectFit="cover"/> */}
-     </div>
-     <div className={classes.contact__contact}>
+      <BackgroundImage classContainer={classes.contact__background} src={'/img/3.jpg'} alt={'Zdjęcie przedstawiające zakochaną parę'} text={''}/>
+     <section className={classes.contact__wrapper}>
       <h2 className={classes.contact__header}>
         KONTAKT
       </h2>
-      <p>Każdego roku wykonuję niewielką liczbę wesel (równo 24!). Każda osoba i historia są wyjątkowe,
+      <p className={classes.contact__description}>Każdego roku wykonuję niewielką liczbę wesel (równo 24!). Każda osoba i historia są wyjątkowe,
          dla tego chcę zwrócić szczególną uwagę na każdego 
         klienta osobno, aby zagwarantować jakość mojej pracy i czasu.</p>
-        <div>
-          <div className={classes.contact__contact}>
-            <p>Email</p>
-            <p>ahjbsdiasd@gmail.com</p>
+        <div  className={classes.contact__contact}>
+          <div className={classes.contact__wrap}>
+            <p className={classes.contact__paragraph}>Email</p>
+            <p className={classes.contact__content}>ahjbsdiasd@gmail.com</p>
           </div>
-          <div>
-            <p>Telefon</p>
-            <p>124141981249</p>
+          <div className={classes.contact__wrap}>
+            <p className={classes.contact__paragraph}>Telefon</p>
+            <p className={classes.contact__content}>+ 48 124 141 981</p>
           </div>
         </div>
-     </div>
+    
      <ContactForm/>
+     </section>
      <Footer/>
     </main>
   );};
