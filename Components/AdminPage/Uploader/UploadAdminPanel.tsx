b9af58a80721bd8acef40502ffa1aaec16381030
Mateusz <m.zaniewski1995@gmail.com>
2023-01-06 21:52:41 +0100

    add admin panel

--- Components/AdminPage/Uploader/UploadAdminPanel.tsx
+++ Components/AdminPage/Uploader/UploadAdminPanel.tsx
@@ -1,7 +1,31 @@
-import {UploadForm} from '../../Form/UploadForm'
-import classes from './UploadAdminPanel.module.css'
+import { FormEvent, useState } from 'react';
+import {PriceChanger} from './Changers/PriceChanger';
+import classes from './UploadAdminPanel.module.css';
+import { UploaderSquare } from './UploaderSquare';
+import {uploaderEdit} from '../../../Data/Data';
 export  const UploadAdminPanel: React.FC = (props) =>{
-    return <div className={classes.uploader}>
-        <UploadForm/>
-    </div>
+    const [divAction, setDivAction] = useState(0);
+    const squareClickHandler = (event:React.MouseEvent<HTMLElement>)=>{
+        const divName = (event.target as HTMLElement).textContent;
+        uploaderEdit.forEach((el,index) => {
+            if(divName === el.text){
+                setDivAction(index+1)
+            }
+        })
+    }
+    console.log(divAction)
+
+    return (
+    <div className={classes.uploader}>
+        <h2 className={classes.uploader__header}>Witaj w Panelu Admina</h2>
+        <p className={classes.uploader__paragraph}>Co pragniesz edytować ? </p>
+        {divAction == 0 && <div className={classes.uploader__wrapper}>
+            {uploaderEdit.map((el)=>{
+                return(
+                <UploaderSquare onClick={squareClickHandler} key={Number(Math.random()*100)} text={el.text}/>
+                );
+            })}
+        </div>}
+        {divAction == 1 && <PriceChanger/>}
+    </div>)
 }
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,6 +1,16 @@
+import { LoginForm } from "../Components/AdminPage/LoginForm/LoginForm"
+import { useContext } from 'react'
+import {AuthContext} from '../Store/Auth-context'
 import { UploadAdminPanel } from "../Components/AdminPage/Uploader/UploadAdminPanel"
-const Admin: React.FC= (props) =>{
-    return <UploadAdminPanel/>
+const Admin: React.FC = () =>{
+    const {loggedIn} = useContext(AuthContext)
+    return (
+        <>
+        {!loggedIn && <LoginForm/>}
+        {loggedIn && <UploadAdminPanel/>}
+        </>
+
+    )
 }
 
 export default Admin
