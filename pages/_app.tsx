c3a7733ec0c0fd59e844f9d9bfe2fbb600d9d09a
Mateusz <m.zaniewski1995@gmail.com>
2023-01-06 21:54:19 +0100

    add auth provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { UiContextProvider } from "../Store/UI-context";
+import { AuthContext, AuthContextProvider } from "../Store/Auth-context";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UiContextProvider>
-      <Component {...pageProps} />;
-    </UiContextProvider>
+    <AuthContextProvider>
+      <UiContextProvider>
+        <Component {...pageProps} />
+      </UiContextProvider>
+    </AuthContextProvider>
+
   );
 }
