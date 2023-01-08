ba0adf6e78296b0b3210e07b28aed61b928a5646
Mateusz <m.zaniewski1995@gmail.com>
2023-01-08 20:38:05 +0100

    optimize sending data to storage and database

--- Components/AdminPage/Uploader/Changers/OpinionChanger.tsx
+++ Components/AdminPage/Uploader/Changers/OpinionChanger.tsx
@@ -5,73 +5,46 @@ import { firebaseFirestore, firebaseStorage } from '../../../../Firebase/firebas
 import {useRef, useState, useEffect, ChangeEvent, InputHTMLAttributes} from 'react';
 import { MutableRefObject } from "react";
 import { InputRef } from '../../../../Types/types';
-import { fileURLToPath } from 'url';
+import { useFirestorage } from '../../../../hooks/useFirestorage';
+import { useFirestoreDatabase } from '../../../../hooks/useFirestoreDatabase';
 export const OpinionChanger = () => {
     // DO ZMIANY ANY ////
-    const [pictureFile,setPictureFile] = useState<any>("");
-    const [pictureURL, setPictureURL] = useState<string>('')
-    let descriptionRef = useRef() as MutableRefObject<HTMLInputElement>
+    const [pictureFile,setPictureFile] = useState<any>(null);
+    const pictureURL = useFirestorage(pictureFile);
+    const [nameOfData, setNameOfData ] = useState<string>('')
+    const [propertiesToSend, setPropertiesToSend ] = useState({})
+    const [databaseLocation, setDatabaseLocation ] = useState<string>("Opinion")
+
+    const descriptionRef = useRef() as MutableRefObject<HTMLInputElement>
     const namesRef = useRef() as MutableRefObject<HTMLInputElement>
     const fileUploadHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
         if(e.target.files != null){
             setPictureFile(e.target?.files[0].name)
         }
     }
-    useEffect(()=>{
-        const uploadFile = () => {
-            const name = new Date().getTime() + String(pictureFile);
-            const storage  =  ref(firebaseStorage, name );
-            const uploadTask = uploadBytesResumable(storage, pictureFile);
-            uploadTask.on('state_changed', (snapshot)=> {
-                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                console.log('Upload is '+ progress + '% done');
-                switch(snapshot.state){
-                    case 'paused':
-                        console.log('Upload is paused');
-                    break;
-                    case 'running':
-                        console.log('Upload is running');
-                    break;
-                    default:
-                    break;
-                }
-            }, (error)=>{
-                console.log(error);
-            },()=>{
-                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
-                    setPictureURL(downloadURL);
-                    console.log(downloadURL)
-                });
-            });
-        }
-        pictureFile&& uploadFile();
-    },[pictureFile])
     const addNewOpinionHandler = async (e:React.SyntheticEvent) => {
         e.preventDefault();
-        const enteredDescriptionRef: InputRef = descriptionRef.current.value
-        const enteredNamesRef: InputRef = namesRef.current.value;
-        console.log(enteredDescriptionRef, enteredNamesRef )
-        try{
-
-
-            await setDoc(doc(firebaseFirestore, "Opinions", enteredNamesRef ), {
-                picture: pictureURL,
-                description: enteredDescriptionRef,
-                names: enteredNamesRef
-            });
-        }catch(err){
-            console.log(err)
-        }
+        const enteredDescriptionRef: InputRef = descriptionRef.current.value.trim()
+        const enteredNamesRef: InputRef = namesRef.current.value.trim();
+        setNameOfData(enteredNamesRef);
+        setPropertiesToSend({
+            name: enteredNamesRef,
+            description: enteredDescriptionRef,
+            url: pictureURL
+        })
     }
+    const {succesfullUpload, error} = useFirestoreDatabase(databaseLocation,propertiesToSend, nameOfData)
     return (
         <div className={classes.opinion}>
+            {succesfullUpload && <p>Udało się dodać nową opinię</p>}
+            {error && <p>Niestety wystąpił błąd</p>}
             <form className={classes.opinion__wrapper} onSubmit={addNewOpinionHandler}>
-                <label className={classes.opinion__label} htmlFor='file'>Załącz zdjęcie</label>
-                <input className={classes.opinion__input} onChange={fileUploadHandler} type="file" id="file" accept='image/png, image/jpeg'/>
-                <label className={classes.opinion__label} htmlFor='description'>Opis</label>
-                <input className={classes.opinion__input} ref={descriptionRef} type="text" id="description"/>
                 <label className={classes.opinion__label} htmlFor='names'>Imiona pary</label>
                 <input className={classes.opinion__input} ref={namesRef} type="text" id="names" />
+                <label className={classes.opinion__label} htmlFor='description'>Opis</label>
+                <input className={classes.opinion__input} ref={descriptionRef} type="text" id="description"/>
+                <label className={classes.opinion__label} htmlFor='file'>Załącz zdjęcie</label>
+                <input className={classes.opinion__input} onChange={fileUploadHandler} type="file" id="file" accept='image/png, image/jpeg'/>
                 <button className={classes.opinion__button} type="submit">Dodaj</button>
             </form>
         </div>
