b78eb9767e043f2259435a3be72fd5d61e4dca62
Mateusz <m.zaniewski1995@gmail.com>
2023-01-13 21:17:10 +0100

    add new component

--- Components/UI/Button.tsx
+++ Components/UI/Button.tsx
@@ -1,15 +1,10 @@
 import classes from "./Button.module.css";
 import Link from "next/link";
 import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
-import { faCalendar } from "@fortawesome/free-solid-svg-icons";
-import { useScrollChecker } from "../../hooks/useScrollChecker";
-import { FontawesomeObject, IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
-export const Button = (props: {text: string, fontAwesome: IconDefinition, path: string}): JSX.Element => {
-  const pageY = useScrollChecker();
+export const Button = (props: {text: string, className:string, path: string}): JSX.Element => {
   return (
-    <Link className={pageY <= 1 ? classes.nav__callendar : `${classes.nav__callendar} ${classes.nav__callendar__scrolled}`} href={props.path}>
-      <FontAwesomeIcon className={classes.callendar__icon} icon={props.fontAwesome} />
+    <Link className={props.className ? props.className : classes.button} href={props.path}>
       {props.text}
     </Link>
   );
