972b3f587f139b84ee6943ada5c72d843c9256aa
Mateusz <m.zaniewski1995@gmail.com>
2023-01-13 21:19:01 +0100

    add child components to Opinion

--- pages/opinion.tsx
+++ pages/opinion.tsx
@@ -1,1 +1,13 @@
-export const Opinion = () => {};
+import { Footer } from '../Components/Footer/Footer';
+import { Header } from './../Components/Header/Header';
+import {ContactFormWithText} from '../Components/Form/ContactFormWithText'
+const Opinion = () => {
+    return (
+        <main>
+          <Header/>
+          <ContactFormWithText text={"POROZMAWIAJMY O TWOIM WYMARZONYM WESELU!"}/>
+          <Footer />
+        </main>
+      );
+};
+export default Opinion
