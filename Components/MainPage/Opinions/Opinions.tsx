99d57a6bd98f54fe82f57d875eecb56485e340b8
Mateusz <m.zaniewski1995@gmail.com>
2023-01-16 22:38:08 +0100

    add counter, and text to slider

--- Components/MainPage/Opinions/Opinions.tsx
+++ Components/MainPage/Opinions/Opinions.tsx
@@ -7,34 +7,35 @@ const opinionsSlider = [{
     src: '/../public/img/picture2.jpg',
     path: '',
     text: 'Mateusz & Wiktoria',
-
+    comment: "Kiedy dostałem zdjęcia, od razu spojrzałem i zapomniałem odpowiedzieć. Jesteśmy bardzo zadowoleni ze wszystkich zdjęć! tak piękne !!  bardzo dziękuję."
 },
 {
     src: '/../public/img/picture3.jpg',
     path: '',
     text: 'Patryk & Agata',
-
+    comment: " razu spojrzałem i zapomniałem odpowiedzieć. Jesteśmy bardzo zadowoleni ze wszystkich zdjęć! tak piękne !!  bardzo dziękuję."
 },
 {
     src: '/../public/img/picture4.jpg',
     path: '',
     text: 'Christina & Emil',
+    comment: " dostałem zdjęcia, od razu spojrzałem i zapomniałem odpowiedzieć. Jesteśmy bardzo zadowoleni ze wszystkich zdjęć! tak piękne !!  bardzo dziękuję."
+
 
 },{
     src: '/../public/img/picture1.jpg',
     path: '',
-    text: 'Christina & Emil',
+    text: 'Christina & Pedał',
+    comment: "Kiedy od razu spojrzałem i zapomniałem odpowiedzieć. Jesteśmy bardzo zadowoleni ze wszystkich zdjęć! tak piękne !!  bardzo dziękuję."
+
 
 }
 ]
 export const Opinions:React.FC = () => {
-    const [slide] = useState(opinionsSlider)
+    const [slide] = useState<{src: string, path: string, text:string, comment:string}[]>(opinionsSlider)
     const [index, setIndex] = useState<number>(0);
-
-    const lastIndex = opinionsSlider.length -1;
-   
     useEffect(() => {
-        const lastIndex = slide.length - 1;
+        const lastIndex:number = slide.length - 1;
         if (index < 0) {
           setIndex(lastIndex);
         }
@@ -44,7 +45,7 @@ export const Opinions:React.FC = () => {
       }, [index, slide]);
     
       useEffect(() => {
-        let slider = setInterval(() => {
+        let slider:ReturnType<typeof setInterval> = setInterval(() => {
           setIndex(index + 1);
         }, 5000);
         return () => {
@@ -55,33 +56,33 @@ export const Opinions:React.FC = () => {
         <div className={classes.slider}>
                <div className={classes.opinions__slider__content}>
                 {slide.map((el, indexSlide)=>{
-                    let position = `${classes.opinions__element__next }`;
-                    if(indexSlide === index){
-                        position = `${classes.opinions__element__active }`
-                    }
-                    if(indexSlide === index - 1 || (index === 0 && indexSlide === slide.length - 1))
-                    {
-                        position = `${classes.opinions__element__last }`
-                    }
-                   
-                    return(   
-                        <>
-                        <article className={`${classes.opinions__element} ${position}`} key={el.src}>
-
-                                <Image
-                                    src={el.src}
-                                    alt='Kamila Koziara'
-                                    layout="fill"
-                                    objectFit="cover"
-                                    className={classes.opinions__image}
-                                    />
+                  let position = `${classes.opinions__element__next }`;
+                  if(indexSlide === index){
+                    position = `${classes.opinions__element__active }`
+                  }
+                  if(indexSlide === index - 1 || (index === 0 && indexSlide === slide.length - 1))
+                  {
+                    position = `${classes.opinions__element__last }`
+                  }
+                  
+                  return(   
+                    <>
+                    <article className={`${classes.opinions__element} ${position}`} key={el.src}>
+                      <p className={classes.opinions__text}>&quot;{opinionsSlider[indexSlide].comment} &quot;</p>
+                      <p className={classes.opinions__names}>{el.text}</p>
+                      <Image
+                          src={el.src}
+                          alt='Kamila Koziara'
+                          layout="fill"
+                          objectFit="cover"
+                          className={classes.opinions__image}
+                          />
                         </article>
                         
                         </>
                         )
                         
                 })}
-                <p className={classes.opinions__text}>{opinionsSlider[0].text}</p>
                 <div className={classes.opinions__navigation}>
                     <FontAwesomeIcon
                     icon={faAngleLeft}
@@ -89,9 +90,7 @@ export const Opinions:React.FC = () => {
                     onClick={() => setIndex(index - 1)}
                     />
                     <div className={classes.opinions__counter}>
-
-                   
-                   
+                    {index + 1} / {slide.length}
                     </div>
                     <FontAwesomeIcon
                     icon={faAngleRight}
