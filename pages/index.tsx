c8f8b54831d0783700e86ddb8a3816790e384a29
Mateusz <m.zaniewski1995@gmail.com>
2023-02-10 16:56:06 +0100

    add all data fetching from firebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,29 @@ import { Questions } from './../Components/MainPage/Questions/Questions';
 import {Opinions} from '../Components/MainPage/Opinions/Opinions'
 import {Portfolio} from '../Components/MainPage/Portfolio/Portfolio';
 import { useFetchFireStore } from './../hooks/useFetchFirestore';
-
+import {useContext} from 'react';
+import { DataContext } from '../Store/Data-context';
 export default function Home() {
+  const {setDataGallery, setDataOffer, setDataOpinion, setDataSlider, setDataPortfolio, dataGallery, dataOffer, dataOpinion, dataPortfolio, dataSlider} = useContext(DataContext);
   const mainSliderData = useFetchFireStore('MainSlider');
-  
+  setDataSlider(mainSliderData);
+  const galleryData = useFetchFireStore('Gallery');
+  setDataGallery(galleryData);
+  const offertData = useFetchFireStore('Offert');
+  setDataOffer(offertData);
+  // const opinionData = useFetchFireStore('Opinion');
+  // setDataOpinion(opinionData);
+  // const portfolioData = useFetchFireStore('Portfolio');
+  // setDataPortfolio(portfolioData);
+console.log(dataSlider);
+console.log(dataGallery);
+// console.log(dataOffer);
+// console.log(dataOpinion);
+// console.log(dataPortfolio);
   return (
     <main>
       <Header black={false}/>
-      <Slider data={mainSliderData} />
+      <Slider/>
       <About />
       <Portfolio/>
       <Opinions/>
