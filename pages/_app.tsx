3ddfdf56d894a776441f5c392df9b1aeeb485c2b
Mateusz <m.zaniewski1995@gmail.com>
2023-02-10 17:06:30 +0100

    delete galleryContextProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,12 @@ import type { AppProps } from "next/app";
 import { UiContextProvider } from "../Store/UI-context";
 import { AuthContext, AuthContextProvider} from "../Store/Auth-context";
 import { DataContextProvider } from "../Store/Data-context";
-import {GalleryContextProvider} from '../Store/Gallery-context'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
       <DataContextProvider>
       <UiContextProvider>
-        <GalleryContextProvider>
         <Component {...pageProps} />
-        </GalleryContextProvider>
       </UiContextProvider>
       </DataContextProvider>
     </AuthContextProvider>
