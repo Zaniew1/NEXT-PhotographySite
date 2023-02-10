af170876d448b898cb4da1beb764e587b7871243
Mateusz <m.zaniewski1995@gmail.com>
2023-02-10 16:55:47 +0100

    add data context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,18 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { UiContextProvider } from "../Store/UI-context";
 import { AuthContext, AuthContextProvider} from "../Store/Auth-context";
+import { DataContextProvider } from "../Store/Data-context";
 import {GalleryContextProvider} from '../Store/Gallery-context'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
+      <DataContextProvider>
       <UiContextProvider>
         <GalleryContextProvider>
         <Component {...pageProps} />
         </GalleryContextProvider>
       </UiContextProvider>
+      </DataContextProvider>
     </AuthContextProvider>
 
   );
