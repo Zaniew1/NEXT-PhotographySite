3e4dd1a424de5d2e8b371259663bfd7739854bf7
Mateusz <m.zaniewski1995@gmail.com>
2023-02-18 23:07:58 +0100

    add single portfolio component

---
+++ pages/portfolio/Agata&Mateusz.tsx
@@ -0,0 +1,25 @@
+import { Footer } from '../../Components/Footer/Footer';
+import { Header } from '../../Components/Header/Header';
+import classes from './portfolio.module.css';
+import { CustomHeader } from '../../Components/UI/CustomHeader';
+import Image from 'next/image';
+
+
+const Weeding = () => {
+    // return (
+    //       <section className={classes.weeding}>
+    //       <Header black={false}/>
+    //         <Image
+    //             src={'/../public/img/1.jpg'}
+    //             alt={''}
+    //             layout="fill"
+    //             objectFit="cover"
+    //             className={classes.weeding__image}
+    //           />
+    //         <CustomHeader customClass={classes.portfolio__header} text={'Portfolio'}/>
+    //         </div>
+    //       <Footer />
+    //       </section>
+    //   );
+};
+export default Weeding
