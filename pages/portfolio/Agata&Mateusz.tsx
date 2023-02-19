3ba872dc5ddae88c9df75444ec64e2adf730b7e5
Mateusz <m.zaniewski1995@gmail.com>
2023-02-19 20:43:26 +0100

    add single portfolio component

--- pages/portfolio/Agata&Mateusz.tsx
+++ pages/portfolio/Agata&Mateusz.tsx
@@ -1,25 +1,37 @@
 import { Footer } from '../../Components/Footer/Footer';
 import { Header } from '../../Components/Header/Header';
-import classes from './portfolio.module.css';
+import classes from './Agata&Mateusz.module.css';
 import { CustomHeader } from '../../Components/UI/CustomHeader';
 import Image from 'next/image';
-
-
+import { portfolioData } from '../../Data/Data';
+import { galleryData } from '../../Data/Data';
+import { GalleryPictures } from '../../Components/GalleryPictures/GalleryPictures';
+import {Portfolio} from '../../Components/MainPage/Portfolio/Portfolio';
+import { ContactFormWithText } from '../../Components/Form/ContactFormWithText';
 const Weeding = () => {
-    // return (
-    //       <section className={classes.weeding}>
-    //       <Header black={false}/>
-    //         <Image
-    //             src={'/../public/img/1.jpg'}
-    //             alt={''}
-    //             layout="fill"
-    //             objectFit="cover"
-    //             className={classes.weeding__image}
-    //           />
-    //         <CustomHeader customClass={classes.portfolio__header} text={'Portfolio'}/>
-    //         </div>
-    //       <Footer />
-    //       </section>
-    //   );
+    let galleryClass:string = '';
+    return (
+        <section className={classes.weeding}>
+            <Header black={false}/>
+            <div className={classes.weeding__image__wrap}>
+                <Image
+                    src={'/../public/img/1.jpg'}
+                    alt={''}
+                    layout="fill"
+                    objectFit="cover"
+                    className={classes.weeding__image}
+                />
+                <p className={classes.weeding__names}>Agata & Mateusz</p>
+            <CustomHeader customClass={classes.weeding__header} text={'Wedding at saint paul de vence'}/>
+            </div>
+            <div className={classes.weeding__description__wrap}>
+                <p className={classes.weeding__description}>This wedding take a place at a small town at St. Paul de Vence. Emil & Christina came from Sweden to celebrate their wedding at family owned villa, followed by the dinner at Michelin restaurant Alain Llorca. The ceremony took place at the green terrace with beautiful view to the city and was made by the Swedish priest. After the ceremony we went to the cocktail, having a lot of fun with their guests and enjoy the time. After that, the party was loud and crazy!</p>
+            </div>
+            <GalleryPictures data={galleryData}/>
+            <Portfolio/>
+            <ContactFormWithText text={'POROZMAWIAJMY O TWOIM WYMARZONYM WESELU!'}/>
+            <Footer />
+        </section>
+      );
 };
 export default Weeding
