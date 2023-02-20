3bd9202bda07f476ef5b1924c9e080353d8ed159
Mateusz <m.zaniewski1995@gmail.com>
2023-02-20 22:21:36 +0100

    add custom Image component

---
+++ Components/UI/CustomImage.tsx
@@ -0,0 +1,17 @@
+
+import classes from './CustomImage.module.css';
+import Image from 'next/image'
+import { CustomImageType } from '../../Types/types';
+export const CustomImage:React.FC<CustomImageType> = (props:CustomImageType): JSX.Element =>{
+    return (
+        <div key={props.key} className={`${props.customClass !== "" ? `${classes.image__wrapper} ${props.customClass}` : classes.image__wrapper}`}>
+            <Image
+            src={props.src}
+            alt={props.alt}
+            layout="fill"
+            objectFit="cover"
+            className={classes.slider__image}
+            />
+        </div>
+    )
+}
