830e5c0db122abcdd23e776e8cf685d78c627283
Mateusz <m.zaniewski1995@gmail.com>
2023-02-24 18:28:55 +0100

    bug fix buttonCallendar

--- Components/MainPage/Questions/Questions.tsx
+++ Components/MainPage/Questions/Questions.tsx
@@ -14,7 +14,7 @@ export const Questions:React.FC  = () => {
                     <h2 className={classes.questions__header__desktop}>MASZ PYTANIE? CZEKAM NA TWOJĄ WIADOMOŚĆ</h2>
                     <p className={classes.questions__paragraph}>Bez względu na cel postaram się stworzyć piękne i ponadczasowe obrazy, które pasują do Państwa postaci. Większość czasu spędziłam w Pradze i Paryżu. Po zdobyciu doświadczenia w różnych częściach Europy fotografuję obecnie w Warszawie.</p>
                     <div className={classes.questions__button__wrapper}>
-                    <ButtonCalendar black={false} text="Sprawdź datę" fontAwesome={faCalendar} path="/contact"/>
+                    <ButtonCalendar black={true} text="Sprawdź datę" fontAwesome={faCalendar} path="/contact"/>
                     </div>
                 </div>
             </div>
