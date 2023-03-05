3354bd1062701bd1a36f1d71baef2496f0061977
Mateusz <m.zaniewski1995@gmail.com>
2023-03-05 22:10:48 +0100

    create new hook to edit elements from firebase

---
+++ hooks/useEditFirestoreDatabase.tsx
@@ -0,0 +1,22 @@
+import {useState, useEffect} from 'react';
+import {firebaseFirestore} from '../Firebase/firebase-config'
+import { doc, updateDoc } from "firebase/firestore";
+export const useEditFirestoreDatabase = (dataLocation:string, passedProperties: {}, isPropertiesReady:boolean, id: string) => {
+    const [succesfullUpload, setSuccesfullUpload] = useState<boolean>(false)
+    const [error, setError] = useState<boolean>(false)
+   useEffect( ()=>{
+    const sendData = async () => {
+        try{
+
+                await updateDoc(doc(firebaseFirestore, dataLocation, id), passedProperties);
+                console.log('edytuję obiekt')
+                setSuccesfullUpload(true);
+        }catch(err){
+            console.log(err)
+            setError(true)
+        }
+    }
+   passedProperties && dataLocation && isPropertiesReady && sendData();
+   },[passedProperties, dataLocation,isPropertiesReady, id])
+   return {succesfullUpload, error}
+}
