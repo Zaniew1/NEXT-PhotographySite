6b25e2eb7fe8a4e7ef231d8b9091931efddaea43
Mateusz <m.zaniewski1995@gmail.com>
2023-03-23 17:44:11 +0100

    fix paths

--- Components/AboutPage/AboutPassion.tsx
+++ Components/AboutPage/AboutPassion.tsx
@@ -5,8 +5,8 @@ export const AboutPassion:React.FC = (): JSX.Element => {
     return(
         <div className={classes.passion__wrapper}>
             <h2 className={classes.passion__header}>HISTORIA DZIEWCZYNY Z PASJĄ I WIELOMA PODRÓŻAMI</h2>
-            <CustomImage customClass={classes.passion__image__wrapper__one} src={'/static/images/kamila2.jpg'} alt={"Zdjęcie przedstawiające Kamilę"}/>
-            <CustomImage customClass={classes.passion__image__wrapper__two} src={'/static/images/kamila3.jpg'} alt={"Zdjęcie przedstawiające pasję Kamili"}/>
+            <CustomImage customClass={classes.passion__image__wrapper__one} src={"/static/images/kamila2.jpg"} alt={"Zdjęcie przedstawiające Kamilę"}/>
+            <CustomImage customClass={classes.passion__image__wrapper__two} src={"/static/images/kamila3.jpg"} alt={"Zdjęcie przedstawiające pasję Kamili"}/>
             <div className={classes.passion__text}>
                 <p className={classes.passion__paragraph}>Zanim zostałam fotografką, pracowałam w sektorze sprzedaży korporacyjnej. Mimo że odnosiłam sukcesy w tej branży przez wiele lat, nie byłam szczęśliwa i w ostateczności odważyłam się, aby zmienić moje życie zawodowe i pasję.</p>
                 <p className={classes.passion__paragraph}>Zawsze interesowałam się fotografią. Jak tylko zmieniłam zawód, kupiłam swój pierwszy aparat. Karierę moją rozpoczęłam jako zawodowa fotografka, fotografując zwykłe sceny uliczne i ludzi.</p>
