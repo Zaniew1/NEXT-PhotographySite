663adf3091bba86084eb6d515b97db2d41396b3f
Mateusz <m.zaniewski1995@gmail.com>
2023-03-25 23:31:46 +0100

    final fix custom image class

--- Components/UI/Images/CustomImage.tsx
+++ Components/UI/Images/CustomImage.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 import { CustomImageType } from '../../../Types/types';
 export const CustomImage:React.FC<CustomImageType> = (props:CustomImageType): JSX.Element =>{
     return (
-        <div className={`${props.customClass != "undefined" ? `${classes.image__wrapper} ${props.customClass}` : classes.image__wrapper}`}>
+        <div className={`${props.customClass !== undefined ? `${classes.image__wrapper} ${props.customClass}` : classes.image__wrapper}`}>
             <Image
             src={props.src}
             alt={props.alt}
             fill
             priority
             style={{objectFit:"cover"}}
-            className={`${props.imageClass != "undefined" ? `${classes.slider__image} ${props.imageClass}` : classes.slider__image}`}
+            className={`${props.imageClass !== undefined ? `${classes.slider__image} ${props.imageClass}` : classes.slider__image}`}
             />
         </div>
     )
