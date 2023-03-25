100e0067fa39ed2db7f35ad122102b2d61fff468
Mateusz <m.zaniewski1995@gmail.com>
2023-03-25 23:46:45 +0100

    set quality different if desktop resolution

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -1,7 +1,7 @@
+import { useContext, Fragment} from "react";
 import { UIContext } from "../../Store/UI-context";
 import { HeaderMobile } from "./HeaderMobile";
 import { HeaderDesktop } from "./HeaderDesktop";
-import { useContext, Fragment} from "react";
 import { blackNav } from "../../Types/types";
 export const Header:React.FC<{black: blackNav}> = (props):JSX.Element =>{
   const { desktopResolution } = useContext(UIContext);
--- Components/UI/Images/CustomImage.tsx
+++ Components/UI/Images/CustomImage.tsx
@@ -1,17 +1,31 @@
 import classes from './CustomImage.module.css';
 import Image from 'next/image'
+import { useContext} from "react";
+import { UIContext } from "../../../Store/UI-context";
 import { CustomImageType } from '../../../Types/types';
 export const CustomImage:React.FC<CustomImageType> = (props:CustomImageType): JSX.Element =>{
+    const { desktopResolution } = useContext(UIContext);
     return (
         <div className={`${props.customClass !== undefined ? `${classes.image__wrapper} ${props.customClass}` : classes.image__wrapper}`}>
+            {desktopResolution ? 
             <Image
             src={props.src}
             alt={props.alt}
+            quality={80}
             fill
             priority
             style={{objectFit:"cover"}}
             className={`${props.imageClass !== undefined ? `${classes.slider__image} ${props.imageClass}` : classes.slider__image}`}
             />
+            : <Image
+            src={props.src}
+            alt={props.alt}
+            quality={40}
+            fill
+            priority
+            style={{objectFit:"cover"}}
+            className={`${props.imageClass !== undefined ? `${classes.slider__image} ${props.imageClass}` : classes.slider__image}`}
+            />}
         </div>
     )
 }
