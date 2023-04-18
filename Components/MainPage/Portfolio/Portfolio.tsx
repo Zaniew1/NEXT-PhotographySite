57d0e7fc7ba15a49cb63a015195ea41f64177be9
Mateusz <m.zaniewski1995@gmail.com>
2023-04-18 18:09:57 +0200

    fix navigation styles in portfolio

--- Components/MainPage/Portfolio/Portfolio.tsx
+++ Components/MainPage/Portfolio/Portfolio.tsx
@@ -4,19 +4,38 @@ import { PortfolioSliderDesktop } from './PortfolioSliderDesktop';
 import {PortfolioSliderMobile} from './PortfolioSliderMobile';
 import { UIContext } from '../../../Store/UI-context';
 import { useContext, useState} from "react";
-import { useFetchFirestore } from '../../../hooks/useFetchFirestore';
+import { SliderNav } from '../../UI/SliderNav/SliderNav';
 import { PortfolioElementType } from '../../../Types/types';
 export const Portfolio = (props:{data:PortfolioElementType[]}) :JSX.Element=>{
     const { desktopResolution } = useContext(UIContext);
-    const [fetchedProperties] = useState<PortfolioElementType[]>(props.data)
+    const [current, setCurrent] = useState<number>(0);
+
+    const [fetchedProperties] = useState<PortfolioElementType[]>(props.data);
+    const previousSlideHandler = () => {
+        if(current == 0){
+          return
+        }
+        else{
+          setCurrent(current+1);
+        }
+      };
+      const nextSlideHandler = () => {
+        if(current < ((-props.data.length)+4)){
+          return
+        }
+        else{
+          setCurrent(current-1);
+        }
+      };
     return (
         <div className={classes.portfolio__wrapper}>
             <div className={classes.portfolio__content}>
                 <p className={classes.portfolio__header}>Reportaż i sesja ślubna</p>
                 <Button text={'Portfolio'} path={'/portfolio'} className={classes.portfolio__button}/>
             </div> 
-            {!desktopResolution && <PortfolioSliderMobile data={fetchedProperties}  />}
-            {desktopResolution && <PortfolioSliderDesktop data={fetchedProperties}/>}
+            {!desktopResolution && <PortfolioSliderMobile data={fetchedProperties}/>}
+            {desktopResolution && <PortfolioSliderDesktop data={fetchedProperties} current={current}/>}
+            {desktopResolution && <SliderNav black={true} index={current} customClass={classes.slider__navigation} length={props.data.length} moveLeft={previousSlideHandler} moveRight={nextSlideHandler} counterInvisible={true}/>}
         </div>
             )
         }
--- Components/MainPage/Portfolio/PortfolioSliderDesktop.tsx
+++ Components/MainPage/Portfolio/PortfolioSliderDesktop.tsx
@@ -1,37 +1,20 @@
 import classes from './PortfolioSliderDesktop.module.css';
 import { CustomImage } from '../../UI/Images/CustomImage';
 import { useState, useRef, useEffect } from "react";
-import { SliderNav } from '../../UI/SliderNav/SliderNav';
 import { MutableRefObject } from 'react';
 import { PortfolioElementType } from '../../../Types/types';
 import { useRouter } from 'next/router';
 
-export const PortfolioSliderDesktop:React.FC<{data:PortfolioElementType[]}> = (props) => {
-    const [current, setCurrent] = useState<number>(0);
+export const PortfolioSliderDesktop:React.FC<{data:PortfolioElementType[], current: number}> = (props) => {
     let containerRef = useRef() as MutableRefObject<HTMLDivElement>
     let carouselRef = useRef() as MutableRefObject<HTMLDivElement>
     const router = useRouter();
-    const previousSlideHandler = () => {
-      if(current == 0){
-        return
-      }
-      else{
-        setCurrent(current+1);
-      }
-    };
-    const nextSlideHandler = () => {
-      if(current < ((-props.data.length)+4)){
-        return
-      }
-      else{
-        setCurrent(current-1);
-      }
-    };
+    
     useEffect(()=>{
         const containerWidth:number = containerRef.current?.offsetWidth;
-      carouselRef.current.style.transform="translate("+(current*containerWidth)+"px, 0)";
+      carouselRef.current.style.transform="translate("+(props.current*containerWidth)+"px, 0)";
         
-      },[current]);
+      },[props]);
 
  return(
     <div className={classes.slider__container}>
@@ -49,7 +32,6 @@ export const PortfolioSliderDesktop:React.FC<{data:PortfolioElementType[]}> = (p
           )
           })}
           </div>
-          <SliderNav black={true} index={current} customClass={classes.slider__navigation} length={props.data.length} moveLeft={previousSlideHandler} moveRight={nextSlideHandler} counterInvisible={true}/>
     </div>
    );
 
